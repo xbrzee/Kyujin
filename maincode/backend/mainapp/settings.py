7ff4c2b8147e4c0e1e33065b8303e9bc6da04175
xbrzee <hibreezo@gmail.com>
2026-04-12 02:10:13 +0545

    add : add new files to the project for better and easier routing

--- maincode/backend/mainapp/settings.py
+++ maincode/backend/mainapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
