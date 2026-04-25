ca14e2dc2636db25f337dd1a88944ee4d381c725
xbrzee <hibreezo@gmail.com>
2026-04-25 22:33:01 +0545

    fix:minor changes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == True;
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kyujin.onrender.com', 'localhost', '127.0.0.1', '.onrender.com']
 
 STATIC_URL = '/static/'
 
