234ba872cdc69f0f7e4ec3045c672bdb0acf9130
xbrzee <hibreezo@gmail.com>
2026-04-25 22:21:43 +0545

    fix:fix CORS HEADACHE

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -132,6 +132,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+     os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000"),
     "http://127.0.0.1:3000",
 ]
